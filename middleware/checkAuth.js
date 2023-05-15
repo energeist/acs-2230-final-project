7cf7356e89ed899cf12183e7f20de95ac817676d
Mark Rattle <mark.rattle@gmail.com>
2023-05-15 12:52:56 -0400

    signup/login routes properly freed from JWT requirement

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -4,14 +4,19 @@ const User = require('../models/user');
 const checkAuth = async (req, res, next) => {
   console.log('Checking authentication');
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET);
-    const user = await User.findById(decodedToken._id)
-    if (user) {
-      req.user = user;
+    if (req.url === '/users/signup' || req.url === '/users/login') {
+      console.log("signup/login does not require JWT")
       next();
     } else {
-      return res.status(400).json({ message: "Invalid JWT, try logging in again." });
+      const token = req.headers.authorization.split(' ')[1];
+      const decodedToken = jwt.verify(token, process.env.SECRET);
+      const user = await User.findById(decodedToken._id)
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(400).json({ message: "Invalid JWT, try logging in again." });
+      }
     }
   } catch(err) {
     return res.status(401).json({ message: "Not Authorized. Please login to continue, or include JWT with your requests." })
